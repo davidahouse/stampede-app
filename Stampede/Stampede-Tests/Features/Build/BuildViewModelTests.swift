3619ec261287304b2eff4799d900f371ada73d55
David House <davidahouse@gmail.com>
2020-10-06 08:32:18 -0400

    :sparkles: Tests for BuildViewModel

---
+++ Stampede/Stampede-Tests/Features/Build/BuildViewModelTests.swift
@@ -0,0 +1,26 @@
+//
+//  BuildViewModelTests.swift
+//  Stampede-Tests
+//
+//  Created by David House on 10/6/20.
+//  Copyright © 2020 David House. All rights reserved.
+//
+
+import XCTest
+@testable import Stampede
+
+class BuildViewModelTests: XCTestCase {
+
+    func testViewModelReturnsAnImageForStatus() {
+        
+        let buildStatuses: [BuildStatus] = [
+            BuildStatus.buildCompletedHoursAgo,
+            BuildStatus.buildStartedMinutesAgo,
+            BuildStatus.someRecentFailedBuild
+        ]
+        for status in buildStatuses {
+            let viewModel = BuildViewModel(buildStatus: status)
+            _ = viewModel.statusImage
+        }
+    }
+}
