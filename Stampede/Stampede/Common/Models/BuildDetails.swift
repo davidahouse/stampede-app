ce0345c581bc12cea0fed799745e0ba9f7eba26e
David House <davidahouse@gmail.com>
2020-11-02 20:14:29 -0500

    :recycle: tweaks to dates in fixtures

--- Stampede/Stampede/Common/Models/BuildDetails.swift
+++ Stampede/Stampede/Common/Models/BuildDetails.swift
@@ -19,13 +19,14 @@ public struct BuildDetails: Codable, Equatable, Hashable {
     public let status: String
     public let started_at: Date
     public let completed_at: Date?
+    public let reference_date: Date?
 
     public var duration: String {
         if let completed = completed_at {
             let interval = completed.timeIntervalSince(started_at)
             return interval.duration()
         } else {
-            let interval = Date().timeIntervalSince(started_at)
+            let interval = (reference_date ?? Date()).timeIntervalSince(started_at)
             return interval.duration()
         }
     }
@@ -34,7 +35,7 @@ public struct BuildDetails: Codable, Equatable, Hashable {
         guard let completed = completed_at else {
             return "still running"
         }
-        return completed.ago()
+        return completed.ago(reference_date ?? Date())
     }
 }
 
@@ -43,16 +44,16 @@ typealias BuildDetailsResponsePublisher = AnyPublisher<[BuildDetails], ServiceEr
 #if DEBUG
 
 extension BuildDetails {
-    public static let activeBuild = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date.fixtureDate.addingTimeInterval(-60*5), completed_at: nil)
-    public static let completedBuild = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 13, status: "success", started_at: Date.fixtureDate.addingTimeInterval(-60*5), completed_at: Date.fixtureDate)
+    public static let activeBuild = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date.fixtureDate.addingTimeInterval(-60*5), completed_at: nil, reference_date: Date.fixtureDate.addingTimeInterval(42))
+    public static let completedBuild = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 13, status: "success", started_at: Date.fixtureDate.addingTimeInterval(-60*5), completed_at: Date.fixtureDate, reference_date: Date.fixtureDate.addingTimeInterval(42))
     
-    public static let buildStartedSecondsAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12), completed_at: nil)
-    public static let buildStartedMinutesAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12*60), completed_at: nil)
-    public static let buildStartedHoursAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12*60*60), completed_at: nil)
+    public static let buildStartedSecondsAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12), completed_at: nil, reference_date: nil)
+    public static let buildStartedMinutesAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12*60), completed_at: nil, reference_date: nil)
+    public static let buildStartedHoursAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12*60*60), completed_at: nil, reference_date: nil)
     
-    public static let buildCompletedSecondsAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12), completed_at: Date().addingTimeInterval(-11))
-    public static let buildCompletedMinutesAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12*60), completed_at: Date().addingTimeInterval(-11*60))
-    public static let buildCompletedHoursAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12*60*60), completed_at: Date().addingTimeInterval(-11*60*60))
+    public static let buildCompletedSecondsAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12), completed_at: Date().addingTimeInterval(-11), reference_date: nil)
+    public static let buildCompletedMinutesAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12*60), completed_at: Date().addingTimeInterval(-11*60), reference_date: nil)
+    public static let buildCompletedHoursAgo = BuildDetails(build_id: "12345", owner: "davidahouse", repository: "some-repository", build_key: "pullrequest-42", build: 12, status: "in_progress", started_at: Date().addingTimeInterval(-12*60*60), completed_at: Date().addingTimeInterval(-11*60*60), reference_date: nil)
 
 }
 
